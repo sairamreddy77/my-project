ebf3d6838dfb7dbfee91161651e99672a7e3bdd4
sairamreddy77 <sairambreddy@gmail.com>
2024-08-02 22:25:29 +0530

    yo

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+const API_URL=import.meta.env.VITE_API_URL
 
 function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('https://your-render-backend-url')
+    fetch('${API_URL}')
       .then(response => response.json())
       .then(data => setMessage(data.message));
   }, []);
