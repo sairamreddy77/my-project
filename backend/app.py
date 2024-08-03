addfd6b7836e2eb6881d625c1d553fcd195eb189
sairamreddy77 <sairambreddy@gmail.com>
2024-08-03 12:43:43 +0530

    Enabled CORS for frontend

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 import logging
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "https://my-project-ashy-eta.vercel.app"}})
+
 
 logging.basicConfig(level=logging.DEBUG)
 
